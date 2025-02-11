678beb0515b691b23d02e10b8dc03d20f8cc08e9
anouther <155570107+anouther@users.noreply.github.com>
2025-02-11 17:37:15 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+    onload = () => {
+      const c = setTimeout(() => {
+        document.body.classList.remove("not-loaded");
+
+        const titles = ('Happy anniversary, Ayin!').split('');
+        const titleElement = document.getElementById('title');
+        let index = 0;
+
+ function play() {
+          //Link Audio Bisa Diganti
+          var audio = new Audio('anchor.mp3');
+          audio.play();
+        }
+
+        function appendTitle() {
+          if (index < titles.length) {
+            titleElement.innerHTML += titles[index];
+            index++;
+            setTimeout(appendTitle, 300); // 300ms delay per huruf
+          } else {
+            // Setelah teks utama selesai, tambahkan subtitle kecil
+            const subtitle = document.createElement('p');
+            subtitle.innerText = "Every moment with you is an incredible blessing. Thank you for being my partner in life, full of love and understanding. I promise to always cherish and love you with all my heart, forever. May our love continue to grow and remain happy forever. I love you more than words can express.";
+            subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
+            subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
+            subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
+
+            // Menambahkan subtitle di bawah #title
+            titleElement.appendChild(subtitle);
+          }
+        }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
+};
